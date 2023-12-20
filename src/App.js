deef1509f607ad6db58ff0e714bd84ce03935a07
Sarika Wankar <sarikaw@excellonsoft.com>
2023-12-20 10:33:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './components/ticTacToe'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <TicTacToe/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
